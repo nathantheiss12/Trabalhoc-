ebca07ce20c32bc45bc486581351800678c6b9bc
Nathan <nathantheiss@gmail.com>
2019-07-22 21:50:07 -0300

    Trabalhinho
    Feito o model,repository e view do estado e da cidade

---
+++ Repository/CategoriaRepository.cs
@@ -0,0 +1,87 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class CategoriaRepository
+    {
+        public List<Categoria> ObterTodos()
+        {
+            SqlCommand comando = Conexao.Conectar();
+            comando.CommandText = @"SELECT * FROM categorias";
+            DataTable tabela = new DataTable();
+            tabela.Load(comando.ExecuteReader());
+
+            List<Categoria> categorias = new List<Categoria>();
+
+            foreach (DataRow linha in tabela.Rows)
+            {
+                Categoria categoria = new Categoria();
+                categoria.Id = Convert.ToInt32(linha["id"]);
+                categoria.Nome = linha["nome"].ToString();
+                categorias.Add(categoria);
+            }
+            comando.Connection.Close();
+            return categorias;
+        }
+
+        public int Inserir(Categoria categoria)
+        {
+            SqlCommand comando = Conexao.Conectar();
+            comando.CommandText = @"INSERT INTO categorias (nome) OUTPUT INSERTED.ID VALUES (@NOME)";
+            comando.Parameters.AddWithValue("@NOME", categoria.Nome);
+            int id = Convert.ToInt32(comando.ExecuteScalar());
+            comando.Connection.Close();
+            return id;
+        }
+
+        public Categoria ObterPeloId(int id)
+        {
+            SqlCommand comando = Conexao.Conectar();
+            comando.CommandText = @"SELECT * FROM categorias WHERE id = @Id";
+            comando.Parameters.AddWithValue("@ID", id);
+
+            DataTable tabela = new DataTable();
+            tabela.Load(comando.ExecuteReader());
+
+            comando.Connection.Close();
+            if (tabela.Rows.Count == 0)
+            {
+                return null;
+            }
+
+            DataRow linha = tabela.Rows[0];
+            Categoria categoria = new Categoria();
+            categoria.Id = Convert.ToInt32(linha["id"]);
+            categoria.Nome = linha["nome"].ToString();
+            return categoria;
+        }
+
+        public bool Alterar(Categoria categoria)
+        {
+            SqlCommand comando = Conexao.Conectar();
+            comando.CommandText = @"UPDATE categorias SET nome = @NOME WHERE id = @ID";
+            comando.Parameters.AddWithValue("@ID", categoria.Id);
+            comando.Parameters.AddWithValue("@NOME", categoria.Nome);
+            int quantidadeAfetada = comando.ExecuteNonQuery();
+            comando.Connection.Close();
+            return quantidadeAfetada == 1;
+        }
+
+        public bool Apagar(int id)
+        {
+            SqlCommand comando = Conexao.Conectar();
+            comando.CommandText = @"DELETE FROM categorias WHERE id = @ID";
+            comando.Parameters.AddWithValue("@Id", id);
+            int quantidadeAfetada = comando.ExecuteNonQuery();
+            comando.Connection.Close();
+            return quantidadeAfetada == 1;
+        }
+    }
+}
---
+++ View/Controllers/UsuarioController.cs
@@ -0,0 +1,68 @@
+﻿using Model;
+using Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace View.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        private UsuarioRepository repository;
+
+        public UsuarioController()
+        {
+            repository = new UsuarioRepository();
+        }
+
+        // GET: Usuario
+        public ActionResult Index()
+        {
+            List<Usuario> usuarios = repository.ObterTodos();
+            ViewBag.Usuarios = usuarios;
+            return View();
+        }
+
+        public ActionResult Cadastro()
+        {
+            return View();
+        }
+
+        public ActionResult Store(string nome, string login, string senha)
+        {
+            Usuario usuario = new Usuario();
+            usuario.Nome = nome;
+            usuario.Login = login;
+            usuario.Senha = senha;
+            repository.Inserir(usuario);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Editar(int id)
+        {
+            Usuario usuario = repository.ObterPeloId(id);
+            ViewBag.Usuario = usuario;
+            return View();
+        }
+
+        public ActionResult Update(int id, string nome, string login, string senha)
+        {
+            Usuario usuario = new Usuario();
+            usuario.Id = id;
+            usuario.Nome = nome;
+            usuario.Login = login;
+            usuario.Senha = senha;
+
+            repository.Alterar(usuario);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Apagar(int id)
+        {
+            repository.Apagar(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
